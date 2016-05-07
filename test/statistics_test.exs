4520079e2cc9d5fe60c04eb41e6ed733a2e27728
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2016-05-08 05:30:14 +1000

    Add normal tests for Statistics

--- test/statistics_test.exs
+++ test/statistics_test.exs
@@ -3,6 +3,10 @@ defmodule Numerix.StatisticsTest do
   use ExCheck
   alias Numerix.Statistics
 
+  test :mean_is_zero_when_list_is_empty do
+    assert Statistics.mean([]) == 0
+  end
+
   property :mean_times_item_count_equals_sum do
     for_all xs in non_empty(list(number)) do
       mean = Float.round(Statistics.mean(xs) * length(xs), 4)
@@ -16,6 +20,10 @@ defmodule Numerix.StatisticsTest do
       mean = Statistics.mean(xs)
       mean >= Enum.min(xs) and mean <= Enum.max(xs)
     end
+  end
+
+  test :median_is_zero_when_list_is_empty do
+    assert Statistics.median([]) == 0
   end
 
   property :median_is_the_middle_value_of_a_sorted_list do
@@ -34,17 +42,27 @@ defmodule Numerix.StatisticsTest do
     end
   end
 
-  property :mode_is_the_most_frequent_value do
+  test :mode_is_nil_when_list_is_empty do
+    assert Statistics.mode([]) == nil
+  end
+
+  property :mode_is_nil_if_no_value_is_repeated do
     for_all xs in non_empty(list(number)) do
-      frequent = [6]
+      xs |> Enum.uniq |> Statistics.mode == nil
+    end
+  end
+
+  property :mode_is_the_most_frequent_value do
+    for_all {x, xs} in {number, non_empty(list(number))} do
+      frequent = [x]
       frequent_list = frequent |> Stream.cycle |> Enum.take(length(xs) + 1)
       xs |> Enum.concat(frequent_list) |> Enum.shuffle |> Statistics.mode == frequent
     end
   end
 
   property :mode_is_the_most_frequent_set_of_values do
-    for_all xs in non_empty(list(number)) do
-      frequent = [666, 777]
+    for_all {x, y, xs} in such_that({x_, y_, xs_} in {number, number, non_empty(list(number))} when x_ < y_) do
+      frequent = [x, y]
       frequent_list = frequent |> Stream.cycle |> Enum.take(2 * (length(xs) + 1))
       xs
       |> Enum.reject(&(Enum.member?(frequent, &1)))
@@ -55,10 +73,8 @@ defmodule Numerix.StatisticsTest do
     end
   end
 
-  property :mode_is_nil_if_no_value_is_repeated do
-    for_all xs in non_empty(list(number)) do
-      xs |> Enum.uniq |> Statistics.mode == nil
-    end
+  test :range_is_zero_when_list_is_empty do
+    assert Statistics.range([]) == 0
   end
 
   property :range_is_the_difference_between_the_largest_and_smallest_values do
