41a2c5aab2de39a7010627d3a776bfd833af7392
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2016-07-15 23:04:44 +1000

    Minor refactor of test data helper to make proper use of File stream

--- test/data_helper.exs
+++ test/data_helper.exs
@@ -1,7 +1,7 @@
 defmodule DataHelper do
 
   def read(dataset) do
-    lines = "test/resources/data/#{dataset}.dat" |> File.read! |> String.split("\n")
+    lines = "test/resources/data/#{dataset}.dat" |> File.stream!
     %{std_dev: parse_std_dev(lines), data: parse_data(lines) }
   end
 
@@ -17,7 +17,7 @@ defmodule DataHelper do
 
   defp parse_data(lines) do
     lines
-    |> Stream.drop_while(fn line -> line != "Data: Y" end)
+    |> Stream.drop_while(fn line -> line != "Data: Y\n" end)
     |> Stream.drop(2)
     |> Stream.map(&String.strip/1)
     |> Stream.map(&Float.parse/1)
