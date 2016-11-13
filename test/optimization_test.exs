85e8a84b81430818cdc3cb1cd53446bf6650ec4a
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2016-11-13 19:06:17 +1100

    Minor reformat of optimization test

--- test/optimization_test.exs
+++ test/optimization_test.exs
@@ -6,9 +6,11 @@ defmodule Numerix.OptimizationTest do
 
   @tag iterations: 10
   property "genetic optimization returns the solution with the lowest cost" do
-    cost_fun = fn(x) -> Enum.sum(x) end
+    cost_fun = fn(xs) -> Enum.sum(xs) end
 
-    for_all {min, max, count} in such_that({min_, max_, _} in {non_neg_integer, non_neg_integer, non_neg_integer} when min_ < max_) do
+    for_all {min, max, count} in such_that({min_, max_, _} in
+      {non_neg_integer, non_neg_integer, non_neg_integer} when min_ < max_) do
+
       domain = [min..max] |> Stream.cycle |> Enum.take(count)
 
       genetic(domain, cost_fun) == [min] |> Stream.cycle |> Enum.take(count)
